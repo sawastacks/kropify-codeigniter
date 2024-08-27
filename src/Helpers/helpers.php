03f6acf8b407d1813b874872343ffb9ef1cc5a7a
MB'DUSENGE Callixte <77852570+mberecall@users.noreply.github.com>
2024-08-27 13:53:42 -0700

    package files path corrected

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
-if( !function_exists('kropifySyles') ){
-    function kropifySyles(){
-        return '<link rel="stylesheet" href="/vendor/sawastacks/kropify/kropify.min.css">';
+if( !function_exists('kropifyStyles') ){
+    function kropifyStyles(){
+        return '<link rel="stylesheet" href="/vendors/sawastacks/kropify/kropify.min.css">';
     }
 }
 
 if( !function_exists('kropifyScripts') ){
     function kropifyScripts(){
-        return '<script src="/vendor/sawastacks/kropify/kropify.min.js"></script>';
+        return '<script src="/vendors/sawastacks/kropify/kropify.min.js"></script>';
 
         
     }
